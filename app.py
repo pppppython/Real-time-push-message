67bc0a257a93bf2bcbbe82b112b45668a5b87344
pppppython <2223601321@qq.com>
2020-04-26 21:13:05 +0800

    代码优化

--- app.py
+++ app.py
@@ -1,29 +1,3 @@
-'''
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-
-@app.route('/',methods=['get'])
-def b():
-    return render_template('b1.html')
-
-
-@socketio.on('message')
-def handle_message(message):
-     send(message, namespace='/chat')
- 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-      emit('my response', json, namespace='/chat')
-
-
-if __name__ == '__main__':
-    socketio.run(app)
-'''
-
 #encoding:utf-8
 #!/usr/bin/env python
 from flask import Flask, render_template
@@ -43,8 +17,6 @@ def index():
 @socketio.on('request_for_response',namespace='/testnamespace')
 def give_response(data):
     value = data.get('param')
-    print('收到前台消息:'+value)
-    #进行一些对value的处理或者其他操作,在此期间可以随时会调用emit方法向前台发送消息
     emit('response',{'code':'200','msg':'春江潮水连海平'})
 
 
